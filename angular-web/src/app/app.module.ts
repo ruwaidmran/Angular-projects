6fa2d8ba1399efc18de8576ee4ddcc81d97fe1a6
Ruwaid Imran <ruwaidmran@gmail.com>
2023-05-07 04:35:05 +0500

    routes

--- angular-web/src/app/app.module.ts
+++ angular-web/src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecondSectionComponent } from './second-section/second-section.component';
@@ -10,7 +12,21 @@ import { HeroBannerComponent } from './hero-banner/hero-banner.component';
 import { HeroContentComponent } from './hero-banner/hero-content/hero-content.component';
 import { HeroImageComponent } from './hero-banner/hero-image/hero-image.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
+import { SectionThirdComponent } from './section-third/section-third.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
 
+
+const appRoutes : Routes=[
+{path:'home',component: HomeComponent},
+{path:'about',component: AboutComponent},
+{path:'gallery',component: GalleryComponent},
+{path:'products',component: ProductsComponent},
+{path:'contact',component: ContactComponent},
+]
 
 @NgModule({
   declarations: [
@@ -22,10 +38,17 @@ import { TopNavComponent } from './top-nav/top-nav.component';
     HeroContentComponent,
     HeroImageComponent,
     TopNavComponent,
+    SectionThirdComponent,
+    HomeComponent,
+    AboutComponent,
+    GalleryComponent,
+    ProductsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-web/src/app/section-third/section-third.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector: 'app-section-third',
+  templateUrl: './section-third.component.html',
+  styleUrls: ['./section-third.component.css']
+})
+
+
+export class SectionThirdComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+  sectionTitle: string = "Interpolation"
+  myName: string = "Ruwaid Imran";
+  fullName: string = "Muhammad"
+  myMethod() {
+    return "my Name is " + this.fullName + this.myName;
+  }
+
+  appStatus: boolean = true;
+  status1: string = "Online";
+  status2: string = "Offline";
+
+}
