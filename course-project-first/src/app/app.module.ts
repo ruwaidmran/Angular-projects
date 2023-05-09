5ed5f25babce552bd9cff2343d156f2a7d4fa4ec
Ruwaid imran <ruwaidmran@gmail.com>
2023-05-09 15:55:01 +0500

    recipe model

--- course-project-first/src/app/app.module.ts
+++ course-project-first/src/app/app.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {  Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesItemsComponent } from './recipes/recipes-list/recipes-items/recipes-items.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes=[
+{path:'recipes', component: RecipesComponent},
+{path:'shopping-list', component: ShoppingListComponent},
+]
 
 
 
@@ -19,11 +28,13 @@ import { RecipesItemsComponent } from './recipes/recipes-list/recipes-items/reci
     RecipesDetailsComponent,
     RecipesListComponent,
     RecipesItemsComponent,
+    ShoppingListComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
